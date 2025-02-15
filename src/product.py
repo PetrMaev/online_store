5fdda63d18e13efccb9cda39b6ac1a36a332ce47
Petr Maev <petr.maev94@gmail.com>
2025-02-15 22:20:46 +0300

    add magical methods for Product and Category

--- src/product.py
+++ src/product.py
@@ -10,6 +10,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
+
+    def __add__(self, other):
+        return self.quantity * self.price + other.quantity * other.price
+
     @classmethod
     def new_product(cls, dict_of_product: dict):
         name = dict_of_product.get('name')
@@ -38,12 +44,21 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, products, total_quantity=0):
         self.name = name
         self.description = description
         self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+        self.total_quantity = total_quantity
+
+    def __str__(self):
+        if self.name == 'Смартфоны':
+            for product in self.__products:
+                self.total_quantity += product.quantity
+            return f'{self.name}, количество продуктов: {self.total_quantity}'
+        else:
+            return 'Другая категория'
 
     def add_product(self, new_product: Product):
         self.__products.append(new_product)
@@ -53,7 +68,7 @@ class Category:
     def products(self):
         product_str = ''
         for product in self.__products:
-            product_str += f'{product.name} {product.price} руб. Остаток: {product.quantity} шт.\n'
+            product_str += f'{str(product)}\n'
         return product_str
 
     @property
@@ -66,30 +81,20 @@ if __name__ == "__main__":
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
+
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3]
     )
 
+    print(str(category1))
+
     print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
 
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5})
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.price = 800
-    print(new_product.price)
-
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
+    print(product1 + product2) # 2 580 000
+    print(product1 + product3)
+    print(product2 + product3)
