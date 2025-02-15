ad89c8a9434b7682f6274edce9d36bd5614f6a50
Petr Maev <petr.maev94@gmail.com>
2025-02-15 22:50:42 +0300

    add tests for new magical methods

--- tests/test_product.py
+++ tests/test_product.py
@@ -18,8 +18,8 @@ def test_category(first_category, second_category):
 
 
 def test_product_list_property(first_category):
-    assert first_category.products == ('Стул 1000.0 руб. Остаток: 4 шт.\n'
-                                       'Стол 5000.0 руб. Остаток: 3 шт.\n')
+    assert first_category.products == ('Стул, 1000.0 руб. Остаток: 4 шт.\n'
+                                       'Стол, 5000.0 руб. Остаток: 3 шт.\n')
 
 
 def test_product_list_setter(first_category, product_for_test):
@@ -30,3 +30,19 @@ def test_product_list_setter(first_category, product_for_test):
 
 def test_price_property(product_for_test):
     assert product_for_test.price == 1000
+
+
+def test_product_str(product_for_test):
+    assert str(product_for_test) == 'Стул, 1000.0 руб. Остаток: 4 шт.'
+
+
+def test_category_str(first_category):
+    assert str(first_category) == 'Другая категория'
+
+
+def test_category_str_2(first_category_phones):
+    assert str(first_category_phones) == 'Смартфоны, количество продуктов: 7 шт.'
+
+
+def test_product_add(product_for_test, product_for_test_2):
+    assert product_for_test + product_for_test_2 == 19000
